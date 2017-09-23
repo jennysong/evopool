f4ba306cc70ad4ff0ed67e0e09939f9d6db17d1e
Jenny Song <jenny.jieun.song@gmail.com>
2017-09-23 16:53:37 -0700

    driver/rider html

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.sendfile('index.html');
 })
 
+app.get('/driver', function (req, res) {
+  res.sendfile('driver.html');
+})
+
+app.get('/rider', function (req, res) {
+  res.sendfile('rider.html');
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
